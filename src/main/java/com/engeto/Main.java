82cc3f681f6d1e9c5c08e6e1d2a900651948cb16
Petr Liszok <liszok@email.cz>
2023-03-29 12:56:10 +0200

    commit

--- src/main/java/com/engeto/Main.java
+++ src/main/java/com/engeto/Main.java
@@ -1,7 +1,19 @@
 package com.engeto;
 
+import com.engeto.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("===Simple calculator===");
+
+        Calculator calculator = new Calculator();
+
+        calculator.add(59,45);
+        calculator.subtract(19,9);
+        calculator.multiply(6,5);
+        calculator.divide(15,5);
+        calculator.squareRoot(64);
+        calculator.square(5);
+        calculator.areaOfCircle(12);
     }
 }
--- src/main/java/com/engeto/calculator/Calculator.java
+++ src/main/java/com/engeto/calculator/Calculator.java
@@ -1,5 +1,61 @@
 package com.engeto.calculator;
 
+import com.engeto.Main;
+
 public class Calculator {
+
+    public long add(long a, long b){
+        long result;
+        result = a + b;
+        System.out.println("The addition is: "+result);
+        return result;
+    }
+
+    public long subtract(int a, int b){
+        long result;
+        result = a - b;
+        System.out.println("The subtraction is: "+result);
+        return result;
+    }
+
+    public long multiply(int a, int b){
+        long result;
+        result = a * b;
+        System.out.println("The multiplication is: "+result);
+        return result;
+    }
+
+    public double divide(double dividend, double divisor)throws ArithmeticException{
+        if(divisor==0){
+            throw new ArithmeticException ("Cannot divide by 0.");
+        }
+        double result;
+        result = dividend / divisor;
+        System.out.println("The division is: "+result);
+        return result;
+    }
+
+    public double squareRoot(double a) throws ArithmeticException{
+        double result;
+        if(a<0) throw new ArithmeticException("Cannot square root negative number.");
+        result = Math.sqrt(a);
+        System.out.println("The square root of your number is: "+result);
+        return result;
+    }
+
+    public double square(double a){
+        double result;
+        result = a * a;
+        System.out.println("The square of your number is: "+result);
+        return result;
+    }
+
+    public double areaOfCircle(double radius) throws ArithmeticException{
+        double result;
+        if(radius<0) throw new ArithmeticException("Cannot compute the circle area of negative number.");
+        result = Math.PI * radius * radius;
+        System.out.println("The circle area is: "+result);
+        return result;
+    }
 
 }
